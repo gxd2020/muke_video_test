e7392dd116a8f0d818dbad4de9a673278cff7ec1
gxd2020 <45885896@qq.com>
2020-06-02 11:23:30 +0800

    这是一个初始化的版本

---
+++ video/config/urls.py
@@ -0,0 +1,12 @@
+# coding:utf-8
+
+from django.contrib import admin
+from django.urls import path,include
+from app.client import urls as client_urls
+from app.dashboard import urls as dashboard_urls
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('client/',include(client_urls)),
+    path('dashboard/',include(dashboard_urls))
+]
